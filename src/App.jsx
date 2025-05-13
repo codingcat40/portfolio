e71cc2fc29d4f91ddd81497947ca24fe5066c380
Naman Matoliya <naman@fieldproxy.com>
2025-05-13 20:41:06 +0530

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [showLoading, setShowLoading] = useState(true);
-  return <div>{showLoading && <LoadingScreen onComplete={() => setShowLoading(false)}/>}</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (<>
+  {showLoading && <LoadingScreen onComplete={() => setShowLoading(false)}/>}
+    <div className={`min-h-screen transition-opacity duration-700
+      ${!showLoading ? "opacity-100" : "opacity-0"}
+      text-gray-200 bg-black`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen = {setMenuOpen}/>
+          
+    </div>
+  </>);
 }
 
 export default App;
